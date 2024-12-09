e9fb5da828843f6e99a177f3112b679c72eb6b68
strahd <jv.palha@gmail.com>
2024-12-09 15:05:16 -0300

    add: cantidate entity to sql table

--- src/main/java/com/victorpalha/vacancy_management/modules/candidate/entities/CandidateEntity.java
+++ src/main/java/com/victorpalha/vacancy_management/modules/candidate/entities/CandidateEntity.java
@@ -1,15 +1,23 @@
 package com.victorpalha.vacancy_management.modules.candidate.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     @Pattern(regexp = "\\S+", message = "The field [USERNAME] should not contain spaces!")
@@ -22,4 +30,7 @@ public class CandidateEntity {
     private String description;
     @URL()
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
