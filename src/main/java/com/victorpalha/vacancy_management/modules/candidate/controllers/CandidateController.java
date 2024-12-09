3ff15b12828c1550971e8c5b11738c1cc04bd3e7
strahd <jv.palha@gmail.com>
2024-12-09 16:17:15 -0300

    add: usecase layer

--- src/main/java/com/victorpalha/vacancy_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/victorpalha/vacancy_management/modules/candidate/controllers/CandidateController.java
@@ -3,8 +3,11 @@ package com.victorpalha.vacancy_management.modules.candidate.controllers;
 import com.victorpalha.vacancy_management.exceptions.UserAlreadyExists;
 import com.victorpalha.vacancy_management.modules.candidate.entities.CandidateEntity;
 import com.victorpalha.vacancy_management.modules.candidate.repository.CandidateRepository;
+import com.victorpalha.vacancy_management.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,19 +19,19 @@ import java.util.Optional;
 @RequestMapping("/candidates")
 public class CandidateController {
 
-    final
-    CandidateRepository candidateRepository;
+    private final CreateCandidateUseCase createCandidateUseCase;
 
-    public CandidateController(CandidateRepository candidateRepository) {
-        this.candidateRepository = candidateRepository;
+    public CandidateController(CreateCandidateUseCase createCandidateUseCase) {
+        this.createCandidateUseCase = createCandidateUseCase;
     }
 
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidate){
-        final Optional<CandidateEntity> candidateAlreadyExists = this.candidateRepository.findByEmailOrUsername(candidate.getEmail(), candidate.getUsername());
-        if (candidateAlreadyExists.isPresent()) {
-            throw new UserAlreadyExists();
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidate){
+        try{
+            CandidateEntity candidateCreated = this.createCandidateUseCase.execute(candidate);
+            return ResponseEntity.status(201).body(candidateCreated);
+        }catch (UserAlreadyExists e){
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
-        this.candidateRepository.save(candidate);
     }
 }
---
+++ src/main/java/com/victorpalha/vacancy_management/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,27 @@
+package com.victorpalha.vacancy_management.modules.candidate.useCases;
+
+import com.victorpalha.vacancy_management.exceptions.UserAlreadyExists;
+import com.victorpalha.vacancy_management.modules.candidate.entities.CandidateEntity;
+import com.victorpalha.vacancy_management.modules.candidate.repository.CandidateRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CreateCandidateUseCase {
+
+    final
+    CandidateRepository candidateRepository;
+
+    public CreateCandidateUseCase(CandidateRepository candidateRepository) {
+        this.candidateRepository = candidateRepository;
+    }
+
+    public CandidateEntity execute(CandidateEntity candidate){
+        final Optional<CandidateEntity> candidateAlreadyExists = this.candidateRepository.findByEmailOrUsername(candidate.getEmail(), candidate.getUsername());
+        if (candidateAlreadyExists.isPresent()) {
+            throw new UserAlreadyExists();
+        }
+        return candidateRepository.save(candidate);
+    }
+}
