c2ea3067637980bce35ae70176363ed14d1bffa3
strahd <jv.palha@gmail.com>
2024-12-15 12:22:21 -0300

    docs: add swagger

--- src/main/java/com/victorpalha/vacancy_management/VacancyManagementApplication.java
+++ src/main/java/com/victorpalha/vacancy_management/VacancyManagementApplication.java
@@ -1,9 +1,18 @@
 package com.victorpalha.vacancy_management;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Vacancy Management",
+                description = "API to connect candidates to companies",
+                version = "1.0.0"
+        )
+)
 public class VacancyManagementApplication {
 
     public static void main(String[] args) {
