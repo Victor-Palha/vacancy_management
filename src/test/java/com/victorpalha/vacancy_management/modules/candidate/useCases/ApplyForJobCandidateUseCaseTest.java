8e14cf75f8e9cb247e31bd3645d53cf3fa6f9c64
strahd <jv.palha@gmail.com>
2024-12-25 19:51:33 -0300

    test: add test to valid if user and job exists when apply for job use case is called

---
+++ src/test/java/com/victorpalha/vacancy_management/modules/candidate/useCases/ApplyForJobCandidateUseCaseTest.java
@@ -0,0 +1,60 @@
+package com.victorpalha.vacancy_management.modules.candidate.useCases;
+
+import com.victorpalha.vacancy_management.exceptions.CandidateNotFound;
+import com.victorpalha.vacancy_management.exceptions.JobNotFound;
+import com.victorpalha.vacancy_management.modules.candidate.entities.CandidateEntity;
+import com.victorpalha.vacancy_management.modules.candidate.repository.CandidateRepository;
+import com.victorpalha.vacancy_management.modules.company.repository.JobRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplyForJobCandidateUseCaseTest {
+
+    @InjectMocks
+    private ApplyForJobCandidateUseCase applyForJobCandidateUseCase;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+
+    @Mock
+    private JobRepository jobRepository;
+
+    @Test
+    @DisplayName("Should not be able to apply for job if candidate doesn't exists")
+    public void should_not_be_able_to_apply_for_job_if_candidate_doesnt_exists() {
+        UUID userId = UUID.randomUUID();
+        UUID jobId = UUID.randomUUID();
+        try{
+            this.applyForJobCandidateUseCase.execute(userId, jobId);
+        }catch (Exception e){
+            assertThat(e).isInstanceOf(CandidateNotFound.class);
+        }
+    }
+
+    @Test
+    @DisplayName("Should not be able to apply for job if job doesn't exists")
+    public void should_not_be_able_to_apply_for_job_if_job_doesnt_exists() {
+        UUID userId = UUID.randomUUID();
+        UUID jobId = UUID.randomUUID();
+        CandidateEntity candidateMock = new CandidateEntity();
+        candidateMock.setId(userId);
+        when(candidateRepository.findById(userId)).thenReturn(Optional.of(candidateMock));
+
+        try{
+            this.applyForJobCandidateUseCase.execute(userId, jobId);
+        }catch (Exception e){
+            assertThat(e).isInstanceOf(JobNotFound.class);
+        }
+    }
+}
